e5f715a69dd550bebf60d2c1ec6d25e3c2dc668f
Sidhant Suman <85753990+SidhantSuman@users.noreply.github.com>
2023-10-26 23:29:32 +0530

    Add files via upload

--- TreeUse[1].cpp
+++ TreeUse[1].cpp
@@ -1,34 +1,6 @@
 #include <iostream>
-#include <queue>
 #include "TreeNode.h"
 using namespace std;
-
-TreeNode<int>* takeInputLevelWise() {
-	int rootData;
-	cout << "Enter root data" << endl;
-	cin >> rootData;
-	TreeNode<int>* root = new TreeNode<int>(rootData);
-
-	queue<TreeNode<int>*> pendingNodes;
-	
-	pendingNodes.push(root);
-	while (pendingNodes.size() != 0) {
-		TreeNode<int>* front = pendingNodes.front();
-		pendingNodes.pop();
-		cout << "Enter num of children of " << front->data << endl;
-		int numChild;
-		cin >> numChild;
-		for (int i = 0; i < numChild; i++) {
-			int childData;
-			cout << "Enter " << i << "th child of " << front->data << endl;
-			cin >> childData;
-			TreeNode<int>* child = new TreeNode<int>(childData);
-			front->children.push_back(child);
-			pendingNodes.push(child);
-		}
-	}
-	return root;
-}
 
 TreeNode<int>* takeInput() {
 	int rootData;
@@ -68,7 +40,7 @@ int main() {
 	root->children.push_back(node1);
 	root->children.push_back(node2);
 	*/
-	TreeNode<int>* root = takeInputLevelWise();
+	TreeNode<int>* root = takeInput();
 	printTree(root);
 	// TODO delete the tree
 }
